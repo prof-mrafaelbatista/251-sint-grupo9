196b90d5bc2b752ae4d3c6ad04099c7b7f760910
MarceloLF <marcelolfzada@gmail.com>
2025-05-30 09:55:27 -0300

    erro no codigo

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import csv
+from os import write
+
 from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def ola():
     # return '<h1>Olá, mundo!</h1>'
     return render_template('index.html')
+
 
 @app.route('/sobre-equipe')
 def sobre_equipe():
@@ -15,7 +19,6 @@ def sobre_equipe():
 
 @app.route('/glossario')
 def glossario():
-
     glossario_de_termos = []
 
     with open('bd_glossario.csv', 'r', newline='', encoding='utf-8') as arquivo:
@@ -25,13 +28,14 @@ def glossario():
 
     return render_template('glossario.html', glossario=glossario_de_termos)
 
+
 @app.route('/novo-termo')
 def novo_termo():
     return render_template('novo_termo.html')
 
+
 @app.route('/criar_termo', methods=['POST'])
 def criar_termo():
-
     termo = request.form['termo']
     definicao = request.form['definicao']
 
@@ -40,6 +44,38 @@ def criar_termo():
         writer.writerow([termo, definicao])
 
     return redirect(url_for('glossario'))
+
+
+@app.route('/excluir_termo/<int:termo_id>', methods=['POST'])
+def excluir_termo(termo_id):
+    with open('bd_glossario.csv', 'r', newline='') as file:
+        reader = csv.reader(file)
+        linhas = list(reader)
+
+    for i, linha in enumerate(linhas):
+        if i == termo_id:
+            del linhas[i]
+            break
+
+    with open('bd_glossario.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(linhas)
+
+    return redirect(url_for('glossario'))
+
+
+@app.route('/editar_termo/<int:termo_id>', methods=['GET'])
+def editar_termo(termo_id):
+    with open('bd_glossario.csv', 'r', newline='') as file:
+        reader = csv.reader(file)
+        linhas = list(reader)
+
+    termo = linhas[termo_id]
+    return render_template('editar.html', termo=termo, termo_id=termo_id)
+
+
+
+
 
 
 app.run()
